11340b974d8313c7d88a9bbfd0878d99a4886e1d
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-01-16 21:41:11 +0600

    Update Clone Graph.cpp

--- Clone Graph.cpp
+++ Clone Graph.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> old2new;
+    
+    Node* cloneGraph(Node* node) {
+        if(!node) return node;
 
+        if(old2new.find(node) != old2new.end())
+            return old2new[node];
+        
+        old2new[node] = new Node(node->val);
+        
+        for(Node* nei : node->neighbors){
+            old2new[node]->neighbors.push_back(cloneGraph(nei));
+        }
+        
+        return old2new[node];
+    }
+};
